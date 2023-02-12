2f55b545f91d8e0763f21aea60368e947f0f4286
professional paster <71851874+lev-droid@users.noreply.github.com>
2023-02-13 08:57:20 +1300

    upload implementation

---
+++ com/work/djkstras/dijkstras.java
@@ -0,0 +1,13 @@
+package com.work.djkstras;
+
+import com.work.helpers.FibonacciHeap;
+import java.io.*;
+import java.util.*;
+
+public final class dijkstras{
+    public static <K> Map<K, Double> closestVector(DirectedGraph<K> graph, K source) {
+
+    }
+
+
+    }
